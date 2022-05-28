be2cbfa41545526e626e7970460abea43e7cfe6e
mimori-kazuki <kaumasa0@icloud.com>
2022-05-28 18:46:55 +0900

    検索機能を実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { MicroCMSListResponse } from "microcms-js-sdk";
 import type { GetStaticProps, NextPage } from "next";
 import Link from "next/link";
+import { ComponentProps, useState } from "react";
 import { client } from "src/libs/client";
 
 export type Blog = {
@@ -9,11 +10,44 @@ export type Blog = {
 };
 
 const Home: NextPage<MicroCMSListResponse<Blog>> = (props) => {
+  const [search ,setSearch ] = useState<MicroCMSListResponse<Blog>>();
+  
+  const handleSubmit: ComponentProps<"form">["onSubmit"] = async(event) => {
+    event.preventDefault();
+    const q = event.currentTarget.query.value;
+    const data = await fetch("/api/search",{
+      method: "POST",
+      headers: {"Content-type": "application/json"},
+      body: JSON.stringify({ q }),
+    })
+    const json: MicroCMSListResponse<Blog> = await data.json()
+    setSearch(json)
+  }
+
+  const handleClick: ComponentProps<"button">["onClick"] = () => {
+    setSearch(undefined)
+  }
+
+  const contents = search ? search.contents : props.contents;
+  const totalCount = search ? search.totalCount : props.totalCount;
+
   return (
     <div>
-      <p className="text-gray-400">{`記事の総数: ${props.totalCount}件`}</p>
+      <form className="flex gap-x-2" onSubmit={handleSubmit}>
+        <input type="text" name="query" className="border border-black p-2"/>
+        <button className="border border-black p-2">検索</button>
+        <button 
+          type="reset" 
+          className="border border-black p-2" 
+          onClick={handleClick}>
+          リセット
+        </button>
+      </form>
+      <p className="text-gray-400">
+        {`${search ? "検索結果" : "記事の総数"}: ${totalCount}件`}
+      </p>
       <ul className="mt-4 space-y-4">
-        {props.contents.map((content) => {
+        {contents.map((content) => {
           return (
             <li key={content.id}>
               <Link href={`/blog/${content.id}`}>
@@ -32,8 +66,9 @@ const Home: NextPage<MicroCMSListResponse<Blog>> = (props) => {
 export const getStaticProps: GetStaticProps<
   MicroCMSListResponse<Blog>
 > = async () => {
-  const data = await client.getList<Blog>({ endpoint: "blog" });
-
+  const data = await client.getList<Blog>({ 
+    endpoint: "blog",
+   });
   return {
     props: data,
   };
